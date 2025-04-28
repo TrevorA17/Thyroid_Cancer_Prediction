f11a60fbf062bd362ea9ec6c14c7a7e8f5fbef51
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-04-28 15:09:07 +0300

    load dataset

---
+++ ExploratoryDataAnalysis.R
@@ -0,0 +1,25 @@
+# Load dataset using read.csv
+ThyroidCancerData <- read.csv("data/thyroid_dataset.csv", colClasses = c(
+  Age = "numeric",
+  Gender = "factor",
+  Smoking = "factor",
+  Hx_Smoking = "factor",
+  Hx_Radiothreapy = "factor",
+  Thyroid_Function = "factor",
+  Physical_Examination = "factor",
+  Adenopathy = "factor",
+  Pathology = "factor",
+  Focality = "factor",
+  Risk = "factor",
+  T = "factor",
+  N = "factor",
+  M = "factor",
+  Stage = "factor",
+  Response = "factor",
+  Recurred = "factor"
+))
+
+# Preview the structure and a few rows
+str(ThyroidCancerData)
+head(ThyroidCancerData)
+View(ThyroidCancerData)
